6efdf90473db9bdf05ac3e91e50256fef6f09abf
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-03-14 06:38:44 -0500

    Add the template for the procedure students are to write.

---
+++ Problem4/src/SimpleListUtils.java
@@ -0,0 +1,27 @@
+import java.util.Comparator;
+
+/**
+ * Utilities for simple lists.
+ */
+public class SimpleListUtils
+{
+  /**
+   * Find the kth largest element of a simple list.
+   *
+   * @pre
+   *   No two values in the list are equal.  We have this precondition
+   *   mostly to make it easier to state the postcondition.
+   * @pre
+   *   k < length of lst
+   * @pre
+   *   lst is not empty
+   * @post
+   *   There are exactly k values smaller than val in the list.  A
+   *   value is "smaller" than val if order.compare(value, val) < 0.
+   */
+  public static <T> T kthSmallest(SimpleList<T> lst, Comparator<T> order, int k)
+  {
+    // STUB
+    return lst.iterator().next();
+  } // kthSmallest(SimpleList<T>, Comparator<T>, int)
+} // class SimpleListUtils
